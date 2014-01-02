2fc73d5358820ae09c479227354b934b0c1d3b3c
CapriciousSage <adam@ithiel.net>
2014-01-02 20:53:55 +1000

    added context menu, escape, and opensubtitles login
    Context Menus require Windows 7 or higher and assumes FileBot is
    installed in C:\Program Files\FileBot. See file comments for more info.

---
+++ escape.cmd
@@ -0,0 +1,5 @@
+:: Calls the 'escape' groovy code
+:: Assumes escape.groovy is located in the cmdlets folder in your FileBot install directory
+
+filebot -script fn:escape
+exit
---
+++ opensubtitles_login.cmd
@@ -0,0 +1,5 @@
+:: FileBot CLI login to OpenSubtitles.org (requires opensubtitles.org account)
+:: It is recommend you create/log into an OpenSubtitles.org account before 
+:: downloading large amounts of subtitles as it increases the anti-leech limit.
+
+filebot -script fn:configure.login
