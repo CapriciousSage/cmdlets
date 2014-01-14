dfe5e61d27ad3daf2e1837b77b9d4f11b1701cec
CapriciousSage <adam@ithiel.net>
2014-01-14 23:02:23 +1000

    Advanced Context Menu v1.3

--- change_subtitle_fetch_language.cmd
+++ change_subtitle_fetch_language.cmd
@@ -1,7 +1,7 @@
 @echo OFF
 
 GOTO EndComment
-	FileBot Context Menu Subtitle Fetch Language Selector v1.0
+	FileBot Context Menu Subtitle Fetch Language Selector v1.1
 
 	Written by CapriciousSage (Ithiel)
 	Requires Filebot to be installed in C:\Program Files\FileBot\
@@ -28,21 +28,101 @@ GOTO EndComment
 	REM --> If error flag set, we do not have admin.
 	if '%errorlevel%' NEQ '0' (
 	    echo Requesting administrative privileges...
-	    goto UACPrompt
-	) else ( goto gotAdmin )
+	    GOTO DirCheck1
+	) else ( GOTO gotAdmin )
 
-	:UACPrompt
-	    echo Set UAC = CreateObject^("Shell.Application"^) > "%~dp0\getadmin.vbs"
-	    set params = %*:"=""
-	    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%~dp0\getadmin.vbs"
+	:DirCheck1
 
-	    "%~dp0\getadmin.vbs"
-	    del "%~dp0\getadmin.vbs"
-	    exit /B
+		copy /Y NUL "%~dp0\.writable" > NUL 2>&1 && set WRITEOK=1
+		IF DEFINED WRITEOK ( 
+			GOTO UACPrompt1
+		 ) else (
+			echo Checking profile instead...
+			GOTO DirCheck2
+		)
+
+	:DirCheck2
+
+		copy /Y NUL "%USERPROFILE%\.writable" > NUL 2>&1 && set WRITEOK=1
+		IF DEFINED WRITEOK ( 
+			GOTO UACPrompt2
+		 ) else (
+			echo Checking temp instead...
+			GOTO DirCheck3
+		)
+
+	:DirCheck3
+
+		copy /Y NUL "%tmp%\.writable" > NUL 2>&1 && set WRITEOK=1
+		IF DEFINED WRITEOK ( 
+			GOTO UACPrompt3
+		 ) else (
+			GOTO UACFailed
+		)
+
+	:UACPrompt1
+
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%~dp0\getadmin.vbs"
+		set params = %*:"=""
+		echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%~dp0\getadmin.vbs"
+
+		"%~dp0\getadmin.vbs"
+
+		if '%errorlevel%' NEQ '0' (
+			del "%~dp0\getadmin.vbs"
+			GOTO DirCheck2
+		) else ( GOTO UACPrompt1Complete )
+
+		:UACPrompt1Complete
+			del "%~dp0\getadmin.vbs"
+			exit /b
+			GOTO gotAdmin
+
+	:UACPrompt2
+
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%USERPROFILE%\getadmin.vbs"
+		set params = %*:"=""
+		echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%USERPROFILE%\getadmin.vbs"
+
+		"%USERPROFILE%\getadmin.vbs"
+
+		if '%errorlevel%' NEQ '0' (
+			del "%USERPROFILE%\getadmin.vbs"
+			GOTO DirCheck3
+		) else ( GOTO UACPrompt2Complete )
+
+		:UACPrompt2Complete
+			del "%USERPROFILE%\getadmin.vbs"
+			exit /b
+			GOTO gotAdmin
+
+	:UACPrompt3
+
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%tmp%\getadmin.vbs"
+		set params = %*:"=""
+		echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%tmp%\getadmin.vbs"
+
+		"%tmp%\getadmin.vbs"
+
+		if '%errorlevel%' NEQ '0' (
+			del "%tmp%\getadmin.vbs"
+			GOTO UACFailed
+		) else ( GOTO UACPrompt3Complete )
+
+		:UACPrompt3Complete
+			del "%tmp%\getadmin.vbs"
+			exit /b
+			GOTO gotAdmin
+
+	:UACFailed
+		echo Upgrading to admin privliages failed.
+		echo Please right click the file and run as administrator.
+		echo PAUSE
+		GOTO FINISH
 
 	:gotAdmin
-	    pushd "%CD%"
-	    CD /D "%~dp0"
+		pushd "%CD%"
+		CD /D "%~dp0"
 	:--------------------------------------
 
 GOTO ASKLANG
--- context_menu_live_installer.cmd
+++ context_menu_live_installer.cmd
@@ -1,7 +1,7 @@
 @echo OFF
 
 GOTO EndComment
-	FileBot Advanced Context Menu v1.2 (Live Installer)
+	FileBot Advanced Context Menu v1.3 (Live Installer)
 	Written by CapriciousSage (Ithiel) with assistance from rednoah (Reinhard Pointner)
 	Requires Windows 7 or higher.
 	This file requires Administrative Privileges
@@ -26,21 +26,101 @@ GOTO EndComment
 	REM --> If error flag set, we do not have admin.
 	if '%errorlevel%' NEQ '0' (
 	    echo Requesting administrative privileges...
-	    goto UACPrompt
-	) else ( goto gotAdmin )
+	    GOTO DirCheck1
+	) else ( GOTO gotAdmin )
 
-	:UACPrompt
-	    echo Set UAC = CreateObject^("Shell.Application"^) > "%~dp0\getadmin.vbs"
-	    set params = %*:"=""
-	    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%~dp0\getadmin.vbs"
+	:DirCheck1
 
-	    "%~dp0\getadmin.vbs"
-	    del "%~dp0\getadmin.vbs"
-	    exit /B
+		copy /Y NUL "%~dp0\.writable" > NUL 2>&1 && set WRITEOK=1
+		IF DEFINED WRITEOK ( 
+			GOTO UACPrompt1
+		 ) else (
+			echo Checking profile instead...
+			GOTO DirCheck2
+		)
+
+	:DirCheck2
+
+		copy /Y NUL "%USERPROFILE%\.writable" > NUL 2>&1 && set WRITEOK=1
+		IF DEFINED WRITEOK ( 
+			GOTO UACPrompt2
+		 ) else (
+			echo Checking temp instead...
+			GOTO DirCheck3
+		)
+
+	:DirCheck3
+
+		copy /Y NUL "%tmp%\.writable" > NUL 2>&1 && set WRITEOK=1
+		IF DEFINED WRITEOK ( 
+			GOTO UACPrompt3
+		 ) else (
+			GOTO UACFailed
+		)
+
+	:UACPrompt1
+
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%~dp0\getadmin.vbs"
+		set params = %*:"=""
+		echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%~dp0\getadmin.vbs"
+
+		"%~dp0\getadmin.vbs"
+
+		if '%errorlevel%' NEQ '0' (
+			del "%~dp0\getadmin.vbs"
+			GOTO DirCheck2
+		) else ( GOTO UACPrompt1Complete )
+
+		:UACPrompt1Complete
+			del "%~dp0\getadmin.vbs"
+			exit /b
+			GOTO gotAdmin
+
+	:UACPrompt2
+
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%USERPROFILE%\getadmin.vbs"
+		set params = %*:"=""
+		echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%USERPROFILE%\getadmin.vbs"
+
+		"%USERPROFILE%\getadmin.vbs"
+
+		if '%errorlevel%' NEQ '0' (
+			del "%USERPROFILE%\getadmin.vbs"
+			GOTO DirCheck3
+		) else ( GOTO UACPrompt2Complete )
+
+		:UACPrompt2Complete
+			del "%USERPROFILE%\getadmin.vbs"
+			exit /b
+			GOTO gotAdmin
+
+	:UACPrompt3
+
+		echo Set UAC = CreateObject^("Shell.Application"^) > "%tmp%\getadmin.vbs"
+		set params = %*:"=""
+		echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%tmp%\getadmin.vbs"
+
+		"%tmp%\getadmin.vbs"
+
+		if '%errorlevel%' NEQ '0' (
+			del "%tmp%\getadmin.vbs"
+			GOTO UACFailed
+		) else ( GOTO UACPrompt3Complete )
+
+		:UACPrompt3Complete
+			del "%tmp%\getadmin.vbs"
+			exit /b
+			GOTO gotAdmin
+
+	:UACFailed
+		echo Upgrading to admin privliages failed.
+		echo Please right click the file and run as administrator.
+		echo PAUSE
+		GOTO FINISH
 
 	:gotAdmin
-	    pushd "%CD%"
-	    CD /D "%~dp0"
+		pushd "%CD%"
+		CD /D "%~dp0"
 	:--------------------------------------
 
 GOTO CALL-SETUP
